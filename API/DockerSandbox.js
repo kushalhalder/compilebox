7dbccd6780913805fa971530cc2e67f3e2992c22
Kushal Halder <krazykushal@gmail.com>
2016-05-05 23:44:11 +0530

    Update DockerSandbox.js

--- API/DockerSandbox.js
+++ API/DockerSandbox.js
@@ -19,79 +19,82 @@
  * @param {String} output_command: Used in case of compilers only, to execute the object code, send " " in case of interpretors
  */
 var DockerSandbox = function(timeout_value, path, folder, vm_name, compiler_name, file_name, code, output_command, languageName, e_arguments, stdin_data) {
-        this.timeout_value = timeout_value;
-        this.path = path;
-        this.folder = folder;
-        this.vm_name = vm_name;
-        this.compiler_name = compiler_name;
-        this.file_name = file_name;
-        this.code = code;
-        this.output_command = output_command;
-        this.langName = languageName;
-        this.extra_arguments = e_arguments;
-        this.stdin_data = stdin_data;
-    };
-    /**
-     * @function
-     * @name DockerSandbox.run
-     * @description Function that first prepares the Docker environment and then executes the Docker sandbox 
-     * @param {Function pointer} success ?????
-     */
+    this.timeout_value = timeout_value;
+    this.path = path;
+    this.folder = folder;
+    this.vm_name = vm_name;
+    this.compiler_name = compiler_name;
+    this.file_name = file_name;
+    this.code = code;
+    this.output_command = output_command;
+    this.langName = languageName;
+    this.extra_arguments = e_arguments;
+    this.stdin_data = stdin_data;
+};
+/**
+ * @function
+ * @name DockerSandbox.run
+ * @description Function that first prepares the Docker environment and then executes the Docker sandbox 
+ * @param {Function pointer} success ?????
+ */
 DockerSandbox.prototype.run = function(success) {
-        var sandbox = this;
-        this.prepare(function() {
-            sandbox.execute(success);
-        });
-    };
-    /*
-     * @function
-     * @name DockerSandbox.prepare
-     * @description Function that creates a directory with the folder name already provided through constructor
-     * and then copies contents of folder named Payload to the created folder, this newly created folder will be mounted
-     * on the Docker Container. A file with the name specified in file_name variable of this class is created and all the
-     * code written in 'code' variable of this class is copied into this file.
-     * Summary: This function produces a folder that contains the source file and 2 scripts, this folder is mounted to our
-     * Docker container when we run it.
-     * @param {Function pointer} success ?????
-     */
+    var sandbox = this;
+    this.prepare(function() {
+        sandbox.execute(success);
+    });
+};
+/*
+ * @function
+ * @name DockerSandbox.prepare
+ * @description Function that creates a directory with the folder name already provided through constructor
+ * and then copies contents of folder named Payload to the created folder, this newly created folder will be mounted
+ * on the Docker Container. A file with the name specified in file_name variable of this class is created and all the
+ * code written in 'code' variable of this class is copied into this file.
+ * Summary: This function produces a folder that contains the source file and 2 scripts, this folder is mounted to our
+ * Docker container when we run it.
+ * @param {Function pointer} success ?????
+ */
 DockerSandbox.prototype.prepare = function(success) {
-        var exec = require('child_process').exec;
-        var fs = require('fs');
-        var sandbox = this;
-        exec("mkdir " + this.path + this.folder + " && cp " + this.path + "/Payload/* " + this.path + this.folder + "&& chmod 777 " + this.path + this.folder, function(st) {
-            var stream = fs.createWriteStream(sandbox.path + sandbox.folder + "/" + sandbox.file_name);
-            stream.on('end', function() {
-                console.log(sandbox.langName + " file was saved!");
-                exec("chmod 777 \'" + sandbox.path + sandbox.folder + "/" + sandbox.file_name + "\'");
-                fs.writeFile(sandbox.path + sandbox.folder + "/inputFile", sandbox.stdin_data, function(err) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("Input file was saved!");
-                        success();
-                    }
-                });
-            });
-            stream.on('error', function() {
-                console.log('error');
+    var exec = require('child_process').exec;
+    var fs = require('fs');
+    var sandbox = this;
+    exec("mkdir " + this.path + this.folder + " && cp " + this.path + "/Payload/* " + this.path + this.folder + "&& chmod 777 " + this.path + this.folder, function(st) {
+        var stream = fs.createWriteStream(sandbox.path + sandbox.folder + "/" + sandbox.file_name);
+        stream.on('end', function() {
+            console.log(sandbox.langName + " file was saved!");
+            exec("chmod 777 \'" + sandbox.path + sandbox.folder + "/" + sandbox.file_name + "\'");
+            fs.writeFile(sandbox.path + sandbox.folder + "/inputFile", sandbox.stdin_data, function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Input file was saved!");
+                    success();
+                }
             });
         });
-    };
-    /*
-     * @function
-     * @name DockerSandbox.execute
-     * @precondition: DockerSandbox.prepare() has successfully completed
-     * @description: This function takes the newly created folder prepared by DockerSandbox.prepare() and spawns a Docker container
-     * with the folder mounted inside the container with the name '/usercode/' and calls the script.sh file present in that folder
-     * to carry out the compilation. The Sandbox is spawned ASYNCHRONOUSLY and is supervised for a timeout limit specified in timeout_limit
-     * variable in this class. This function keeps checking for the file "Completed" until the file is created by script.sh or the timeout occurs
-     * In case of timeout an error message is returned back, otherwise the contents of the file (which could be the program output or log of 
-     * compilation error) is returned. In the end the function deletes the temporary folder and exits
-     * 
-     * Summary: Run the Docker container and execute script.sh inside it. Return the output generated and delete the mounted folder
-     *
-     * @param {Function pointer} success ?????
-     */
+        stream.on('error', function() {
+            console.log('error');
+        });
+        request.get(sandbox.code).on('error', function(err) {
+            console.log(err);
+        }).pipe(stream);
+    });
+};
+/*
+ * @function
+ * @name DockerSandbox.execute
+ * @precondition: DockerSandbox.prepare() has successfully completed
+ * @description: This function takes the newly created folder prepared by DockerSandbox.prepare() and spawns a Docker container
+ * with the folder mounted inside the container with the name '/usercode/' and calls the script.sh file present in that folder
+ * to carry out the compilation. The Sandbox is spawned ASYNCHRONOUSLY and is supervised for a timeout limit specified in timeout_limit
+ * variable in this class. This function keeps checking for the file "Completed" until the file is created by script.sh or the timeout occurs
+ * In case of timeout an error message is returned back, otherwise the contents of the file (which could be the program output or log of 
+ * compilation error) is returned. In the end the function deletes the temporary folder and exits
+ * 
+ * Summary: Run the Docker container and execute script.sh inside it. Return the output generated and delete the mounted folder
+ *
+ * @param {Function pointer} success ?????
+ */
 DockerSandbox.prototype.execute = function(success) {
     var exec = require('child_process').exec;
     var fs = require('fs');
@@ -104,7 +107,7 @@ DockerSandbox.prototype.execute = function(success) {
     //execute the Docker, This is done ASYNCHRONOUSLY
     exec(st);
     console.log("------------------------------");
-        //Check For File named "completed" after every 1 second
+    //Check For File named "completed" after every 1 second
     var intid = setInterval(function() {
         //Displaying the checking message after 1 second interval, testing purposes only
         //console.log("Checking " + sandbox.path+sandbox.folder + ": for completion: " + myC);
@@ -118,7 +121,7 @@ DockerSandbox.prototype.execute = function(success) {
             //if file is found simply display a message and proceed
             else if (myC < sandbox.timeout_value) {
                 console.log("DONE");
-                    //check for possible errors
+                //check for possible errors
                 fs.readFile(sandbox.path + sandbox.folder + '/errors', 'utf8', function(err2, data2) {
                     if (!data2) data2 = "";
                     console.log("Error file: ");
